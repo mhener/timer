237b1031238873d6c2570ed069b77d3a8f7c8660
Mardin Hener <mardinhener14@gmail.com>
2022-07-05 19:26:11 +0000

    fixed to accomodate for an edge case

--- timer1.js
+++ timer1.js
@@ -2,9 +2,11 @@ const alarmTime = process.argv.slice(2);
 const timer = function() {
   for (let num of alarmTime) {
     let timeOut = 1000;
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, timeOut *= num);
+    if (num > 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, timeOut *= num);
+    }
   }
 };
 
